36c13799faa72028b99096e2ac99cf417467688d
717abishekneupane <717abishekneupane@gmail.com>
2025-03-08 02:21:09 +0000

    back to google idx

--- day14/index.php
+++ day14/index.php
@@ -2,7 +2,8 @@
 
     $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
     echo "URI: " . $uri;  // Debugging line
-
+    // Just practising routing.
+    
     switch($uri) {
         case '/about':
             include 'includes/about.php';
@@ -10,9 +11,11 @@
         case '/contact':
             include 'includes/contact.php';
             break;
-        case '/detail.php':
+        case '/detail':
             include 'includes/detail.php';
             break;
+        case '/logout':
+            include 'includes/logout.php'
         default: 
             echo "Page not found!";
             break;
